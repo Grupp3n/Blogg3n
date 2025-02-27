0893cd8373db7c256d301e8331be5a286ee73746
gohyakkuen <0308aica@gapp.uddevalla.se>
2025-02-27 11:26:15 +0100

    Ja

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>SKOLA</title>
+</head>
+<body>
+    <header>
+        <h1>BLOGG</h1>
+    </header>
+
+    <div class="container">
+        <div class="container_inside">
+            <p>DETTA ÄR LITE TEXT</p>
+        </div>
+    </div>
+
+    
+</body>
+</html>
