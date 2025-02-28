f27bbb1d42767f673211c86ee4e168a60a4e6a26
wapalo <b22phila@student.his.se>
2025-02-28 15:15:00 +0100

    fixed login knapp

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $thumbnail_posts = $stmt_thumbnails->fetchAll(PDO::FETCH_ASSOC);
         <!-- Log in "button" if SESSION inloggad är false -->
         <img src="img/transparent logo.png" alt="Nexlify" class="Logo">
         <?php if (isset($_SESSION['INLOGGAD']) && $_SESSION['INLOGGAD'] === false) : ?>
-            <a href="#adPopup" class="ad-link">Log in</a>
+            <a href="#adPopup" class="Loginknapp">Log in</a>
         <?php else : ?>
         <?php endif; ?>
 
